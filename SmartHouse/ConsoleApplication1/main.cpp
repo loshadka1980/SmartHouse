0543393f086b0b2658b9f8edf2d7c08158963256
loshadka1980 <roschin001@ramble.ru>
2015-05-31 17:45:22 +0300

    "Hello World" app to test

---
+++ SmartHouse/ConsoleApplication1/main.cpp
@@ -0,0 +1,15 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <conio.h>
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	std::cout << "Hello, world!" << std::endl;
+	_getch();
+	return 0;
+}
+
